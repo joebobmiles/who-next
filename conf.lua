7f3266bae65f725c8304b5b23aba531bc45147bc
Joseph R Miles <joe@jrm.dev>
2022-05-14 19:42:02 -0700

    feat: added app icon

--- conf.lua
+++ conf.lua
@@ -1,11 +1,10 @@
 function love.conf(t)
-    t.window.title = "Who Next?"
-    t.window.icon = nil
+    t.window.title = "Who Next"
+    t.window.icon = "asset/icon.png"
     t.window.height = 200
     t.window.width = 800
 
     t.modules.audio = false
-    t.modules.image = false
     t.modules.joystick = false
     t.modules.physics = false
     t.modules.sound = false
